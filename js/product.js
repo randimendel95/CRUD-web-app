7a4df1a04df3c2305239bcc1ee9e8f23321a6a08
Randi <rlmendel@uw.edu>
2015-10-19 10:59:50 -0700

    inlabchanges

---
+++ js/product.js
@@ -0,0 +1,6 @@
+
+//initialize application on parse
+Parse.initialize("AAEvBWGQojHwY1KfpTA2VU74RieRMwMpITkrWZxD", "e4SrlzeNe9rYdyc7HEPDhD3lX8FpUqXDc9Fp8GMP");
+
+//new sub class on Parse
+var Reviews = Parse.Object.extend('Reviews');
