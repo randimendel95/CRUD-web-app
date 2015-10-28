c39cbd93db8e1f593538eaedb3c38194a85fd8c9
Randi Mendel <rlmendel@yahoo.com>
2015-10-28 00:09:20 -0700

    work

--- js/product.js
+++ js/product.js
@@ -4,15 +4,17 @@ Parse.initialize("zvjivvaJkAsKdlqXeuuG81uaOMxhqsj61i2JpDum", "XrvD0ospedgpnedUDO
 
 //new sub class of parse 
 var PugReview = Parse.Object.extend('PugReview');
+var numReviews;
+var totalScore;
 
-var review = new PugReview();
-var totalScore = 0;
-var numReviews = 0;
+var upVotes;
+var downVotes;
 
-//print stars
+//print stars for user
 $('#rating').raty({
 	cancel:false
 });
+
 
 //when submitting review
 $('form').submit(function() {
@@ -26,10 +28,14 @@ $('form').submit(function() {
 	var rating = $('#rating').raty('score');
 
 	review.set('rating', rating);
+	review.set('upVotes',0);
+	review.set('downVotes',0);
 
 	//save instance of pug review (review)
 	review.save(null, {
+
 		success: getData,
+		//tells you what errors you have in code if any
 		error: function(data, error) {
 			console.log(data);
 			console.log(error);
@@ -41,6 +47,7 @@ $('form').submit(function() {
 });
 
 var getData = function() {
+	console.log('getData');
 
 	//new query for PugReview class to get data from user reviews 
 	var query = new Parse.Query(PugReview);
@@ -52,6 +59,7 @@ var getData = function() {
 	query.find({
 		success:buildList
 	});
+
 }
 
 //to build list of reviews
@@ -59,15 +67,24 @@ var buildList = function(data) {
 	//create list 
 	console.log('buildList',data); 
 
+	numReviews = data.length;
+	totalScore = 0;
+
 	//empty out list 
 	$('#list').empty();
-
 
 	data.forEach(function(d) {
 		addItem(d)
 	});
-	
+	//print average review stars 
+	$('#averageRating').raty({
+		score: totalScore / numReviews,
+		readOnly: true
+	});
+
+
 }
+
 
 // This function takes in an item, adds it to the screen
 var addItem = function(item) {
@@ -75,16 +92,36 @@ var addItem = function(item) {
 	var curTitle = item.get('reviewTitle');
 	var curReview = item.get('reviewContent');
 	var curScore = item.get('rating');
-
-	var curInfo = '<div><p>' + curTitle + '</p></div><div><p>' + curReview + '</p></div><div id="rating'+item.id+'"></div>';
+	totalScore += curScore;
+	//upvotes = item.get('upVotes');
+	//downvotes = item.get('downVotes');
+	
+	var curInfo = '<div id="rating'+item.id+'"></div>' + '<div><p id="title'+item.id+'"></p></div>' + '<div><p id="review'+item.id+'"></p></div>';
 	// Append li that includes text from the data item
 	$('#list').append(curInfo);
+	$('#title'+item.id).text(curTitle);
+	$('#rating'+item.id).text(curReview);
 	$('#rating'+item.id).raty({
 		score:curScore,
 		readOnly: true
 	});
+	$('#list').append('Was this helpful? <button id = "upvote'+item.id+'"><i class="fa fa-thumbs-up"></i></button>' + '<button id = "downvote'+item.id+'""><i class="fa fa-thumbs-down"></i>');
 	
-}
+	$('#upvote'+item.id).click(function() {
+		item.increment('upVotes');
+		item.save(null, {
+			success: getData
+		});
+	})
 
-// Call your getData function when the page loads
+	$('#downvote'+item.id).click(function() {
+		item.increment('downVotes');
+		item.save(null, {
+			success: getData
+		});
+	})
+
+	$('#list').append('<p>' + item.get('upVotes') + ' out of ' + (item.get('upVotes')+ item.get('downVotes')) + ' people found this review helpful</p>');
+}
+//when it's done loading the page, get data!
 getData();
